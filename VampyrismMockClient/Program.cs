02c0a14f25c3499e833beb7dea379ee0525af7f5
novium <mail@novium.pw>
2020-11-15 16:48:20 +0100

    Add project files.

---
+++ VampyrismMockClient/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace VampyrismMockClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UDPClient c = new UDPClient("127.0.0.1", 25565);
+            c.Init();
+        }
+    }
+}
---
+++ VampyrismMockClient/UDPClient.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace VampyrismMockClient
+{
+    class UDPClient
+    {
+        IPEndPoint recv;
+        String host;
+        int port;
+
+        UdpClient c;
+
+        public UDPClient(String host, int port)
+        {
+            this.host = host;
+            this.port = port;
+        }
+
+        public void Init()
+        {
+            this.c = new UdpClient();
+            this.c.Connect("localhost", 9000);
+            this.c.Send(Encoding.ASCII.GetBytes("ping"), Encoding.ASCII.GetBytes("ping").Length);
+        }
+
+        private void RecieveCallback(IAsyncResult result)
+        {
+
+        }
+    }
+}
